7267d5fabd22e3a073b8be7f1dfb905e3bd15804
rbuccat <richbuccat@yahoo.com>
2016-10-10 14:34:20 -0700

    modified js and index files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,6 +3,7 @@
 	var winWin = 0;
 	var loseLose = 0;
 	var numGuesses = 9;
+	var guessWrong = [];
 	// Create code to randomly choose one of the letters in the alphabet (Computer) 
 	var computerGuess = alphabetInArray[Math.floor(Math.random()*alphabetInArray.length)];
 
@@ -19,32 +20,38 @@
 	console.log(userGuess);
 	console.log(computerGuess);
 
-
 	if (userGuess == computerGuess) {
 		winWin++;
 		numGuesses = 9;
+		guessWrong = [];
 		computerGuess = alphabetInArray[Math.floor(Math.random()*alphabetInArray.length)];
+		
 		document.getElementById("win").innerHTML = winWin;
 		document.getElementById("guess").innerHTML = numGuesses;
 		document.getElementById("history").innerHTML = null;
+		alert("You Won! '" + userGuess + "' was my guess.");
 		console.log("win " + winWin);
 	}
 	else if (userGuess != computerGuess	&& numGuesses === 1) {
 		loseLose++;
 		numGuesses = 9;
+		guessWrong = [];
 		computerGuess = alphabetInArray[Math.floor(Math.random()*alphabetInArray.length)];
 		
 		document.getElementById("loss").innerHTML = loseLose;
 		document.getElementById("guess").innerHTML = numGuesses;
 		document.getElementById("history").innerHTML = null;
+		alert("You Lost!");
 		console.log("loss " + loseLose);
 	}
+	else if(alphabetInArray.indexOf(userGuess) == -1) {
+			alert("Please choose a letter between A-Z. Try again");
+		}
 	else if (userGuess != computerGuess) {
 		numGuesses--;
-		var guessWrong = document.createTextNode(userGuess + ", "); 
-		document.getElementById("history").appendChild(guessWrong);
+		guessWrong.push(userGuess);
+		document.getElementById("history").innerHTML = guessWrong.join();
 		document.getElementById("guess").innerHTML = numGuesses;
-  		
 		console.log("history " + userGuess);
 		console.log("guess " + numGuesses);
 	}
